a031aa18dabfbd3ba2bd0ad1d91412da70e318b2
James Batsell <jamesbatsell05@gmail.com>
2020-03-02 20:31:21 +0000

    Automatically backed up by Learn

---
+++ bin/artist.rb
@@ -0,0 +1,46 @@
+class Artist 
+  extend Concerns::Findable
+  
+  attr_accessor :name, :songs
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @songs = [ ]
+  end
+  
+def self.all
+  @@all
+end
+
+def self.destroy_all
+  @@all.clear
+end
+
+def save 
+  @@all << self
+end
+
+
+def self.create(name)
+ newest = Artist.new(name)
+  newest.save
+  newest
+end
+
+def add_song(song)
+  
+song.artist = self if !song.artist 
+self.songs << song if !self.songs.include?(song)
+  #binding.pry
+#Song.name(artist)
+  
+end
+
+ def genres 
+   songs.collect do |stuff|
+     stuff.genre
+   end.uniq
+ end
+
+end
